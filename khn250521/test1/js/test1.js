ae44f5d4334dd4e6beffbae9883206a280b590bb
kimhana415 <khnaa415@gmail.com>
2025-05-21 16:49:20 +0900

    250521 데일리 미션

---
+++ khn250521/test1/js/test1.js
@@ -0,0 +1,5 @@
+// 250521khn-인스타그램 모달
+function closeModal(){
+  document.getElementById("modal").classList.add("hidden");
+  document.getElementById("modal-overlay").classList.add("hidden");
+}
---
+++ khn250521/test2/js/test2.js
@@ -0,0 +1,11 @@
+// 250521khn-인스타그램
+function heartClick(element){
+  element.classList.toggle("heart-red");
+  element.classList.toggle("bi-heart-fill");
+  element.classList.toggle("bi-heart");
+}
+
+function bookmarkClick(element){
+  element.classList.toggle("bi-bookmark-fill");
+  element.classList.toggle("bi-bookmark");
+}
